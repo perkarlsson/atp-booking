04ff22615ec356f46a060fa7aa3912a052a1399f
nideas <per@pkarlsson.com>
2019-08-18 15:52:50 -0700

    fixed form

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,8 @@ const IndexPage = () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
       >
+      <input type="hidden" name="bot-field"/>
+      <input type="hidden" name="form-name" value="contact"/>
       <input name="name" placeholder="Your Name" type="text"/>
       <button>Send</button>
     </form>
